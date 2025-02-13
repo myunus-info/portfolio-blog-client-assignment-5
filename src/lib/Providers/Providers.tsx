5a2da2ab259282128c7a79c2b5332624d306609b
Muhammad Yunus <myunus_info@yahoo.com>
2025-02-13 16:28:00 +0200

    Removed Unnecessary File

--- src/lib/Providers/Providers.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-import { Toaster } from 'sonner';
-
-const Providers = ({ children }: { children: ReactNode }) => {
-  return <Toaster>{children}</Toast>;
-};
-
-export default Providers;
